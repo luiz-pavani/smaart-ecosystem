771149bbb18a4ed2883a23715139aa28e902854d
Luiz Pavani <luizpavani@gmail.com>
2026-03-25 09:24:28 -0300

    chore: bump to Beta 82

--- apps/titan/lib/version.ts
+++ apps/titan/lib/version.ts
@@ -3,10 +3,10 @@
  * Auto-updated with each release
  */
 
-export const APP_VERSION = '81.0.0'
-export const APP_VERSION_NAME = 'Beta 81'
+export const APP_VERSION = '82.0.0'
+export const APP_VERSION_NAME = 'Beta 82'
 export const APP_RELEASE_DATE = '2026-03-25'
-export const APP_BUILD = 'Backnumber official backgrounds - Beta 81'
+export const APP_BUILD = 'Backnumber lote system + JPG export - Beta 82'
 
 export const getVersionString = () => {
   return `${APP_VERSION_NAME} (v${APP_VERSION})`
